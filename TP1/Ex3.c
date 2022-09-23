c862a51ec186d936bd379456a47656a780d1f312
Ramos Luis <luismbonito@gmail.com>
2022-09-23 16:17:01 +0200

    Exercice 3 TP1 Prog Avancée en C

--- TP1/Ex3.c
+++ TP1/Ex3.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char * argv[]){
-  
+int pgcd(int a, int b) {
+  if (b == 0) {
+    return a;
+  }
+  return pgcd(b, a % b);
+}
+
+int ppcm(int a, int b) { return (a * b) / pgcd(a, b); }
+
+int main(int argc, char *argv[]) {
+  int i;
+  int premier_ppcm;
+  int boucle_ppcm;
+  if (argc < 2) {
+    printf("Pas assez d'arguments\n");
+    return 0;
+  }
+  premier_ppcm = ppcm(atoi(argv[1]), atoi(argv[2]));
+  for (i = 3; i < argc; i++) {
+    boucle_ppcm = ppcm(premier_ppcm, atoi(argv[i]));
+    premier_ppcm = boucle_ppcm;
+  }
+  printf("%d\n", boucle_ppcm);
   return 0;
 }
